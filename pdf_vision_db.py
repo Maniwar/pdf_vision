5f34f2a2be4611ccb2db5cef6bd19d2d6e4ff468
Maniwar <mberenji@gmail.com>
2025-01-10 17:32:15 -0600

    Update pdf_vision_db.py

--- pdf_vision_db.py
+++ pdf_vision_db.py
@@ -36,7 +36,7 @@ st.set_page_config(layout="wide")
 
 # Set the API key using st.secrets for secure access
 os.environ["OPENAI_API_KEY"] = st.secrets["general"]["OPENAI_API_KEY"]
-MODEL = "gpt-o1"  # Latest GPT-4 Turbo model
+MODEL = "o1"  # Latest GPT-4 Turbo model
 MAX_TOKENS = 16000
 client = OpenAI()
 embeddings = OpenAIEmbeddings()
